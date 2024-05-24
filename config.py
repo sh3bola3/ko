218113b4643d8475692ce168a4b2299e2a5bc33c
kingookk <138496905+kingookk@users.noreply.github.com>
2024-05-24 17:09:40 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 2000))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6130127477))
+OWNER_ID = int(getenv("OWNER_ID", 1400467850))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
