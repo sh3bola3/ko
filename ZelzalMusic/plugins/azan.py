e9f57a448876721ca8c36a24374e39eb940bb2a4
kingookk <138496905+kingookk@users.noreply.github.com>
2024-06-05 00:19:30 +0300

    Add files via upload

--- ZelzalMusic/plugins/azan.py
+++ ZelzalMusic/plugins/azan.py
@@ -2,16 +2,16 @@ import asyncio
 from pyrogram import filters
 from pyrogram.errors import FloodWait
 from pyrogram.raw import types
-from AnonX import app
+from ZelzalMusic import app
 import random
 from datetime import datetime
 import requests
 import pytz
-from AnonX.core.call import Anon
+from ZelzalMusic.core.call import Anon
 from pytgcalls import PyTgCalls, StreamType
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from AnonX.core.call import Anon
-from AnonX.utils.database import *
+from ZelzalMusic.core.call import Anon
+from ZelzalMusic.utils.database import *
 from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError,AlreadyJoinedError)
 from pyrogram.errors import (
     ChatAdminRequired,
@@ -45,7 +45,7 @@ async def kill():
 
 async def play(i):
   assistant = await group_assistant(Anon,i)
-  file_path = "./AnonX/assets/azan.m4a"
+  file_path = "./ZelzalMusic/assets/azan.m4a"
   stream = AudioPiped(file_path, audio_parameters=HighQualityAudio())
   try:
       await assistant.join_group_call(
