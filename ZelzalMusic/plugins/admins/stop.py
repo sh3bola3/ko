8e1f8f59ddf8240a11ba13e17f4ec768fd1db09a
kingookk <138496905+kingookk@users.noreply.github.com>
2024-06-06 13:04:16 +0300

    Add files via upload

--- ZelzalMusic/plugins/admins/stop.py
+++ ZelzalMusic/plugins/admins/stop.py
@@ -10,11 +10,26 @@ from ZelzalMusic.utils.decorators import AdminRightsCheck
 from ZelzalMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
-
+from ZelzalMusic.utils.decorators.adminss import AdminRightsCheckk
 @app.on_message(
-    command(["/stop", "اخرص", "اسكت", "ايقاف"]) & ~BANNED_USERS
+    command(["/stop", "اخرص", "اسكت", "ايقاف"]) & ~BANNED_USERS & filters.group
 )
 @AdminRightsCheck
+async def stop_music(cli, message: Message, _, chat_id):
+    if " " in message.text:
+        return
+    await Zelzaly.stop_stream(chat_id)
+    await set_loop(chat_id, 0)
+    user_mention = message.from_user.mention if message.from_user else "المشـرف"
+    await message.reply_text(
+        _["admin_5"].format(user_mention), reply_markup=close_markup(_)
+    )
+
+
+@app.on_message(
+    command(["/stop", "اخرص", "اسكت", "ايقاف"]) & ~BANNED_USERS & filters.channel
+)
+@AdminRightsCheckk
 async def stop_music(cli, message: Message, _, chat_id):
     if " " in message.text:
         return
